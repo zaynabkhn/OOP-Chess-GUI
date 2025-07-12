d66b7f87f4d7634a97640d96eaf3a92b6b34fd6a
Your Name <you@example.com>
2025-07-12 16:00:26 -0500

    Add Rook and Bishop implementation

--- Pieces/Bishop.java
+++ Pieces/Bishop.java
@@ -1,17 +1,50 @@
+
 package Pieces;
 
-public class Bishop extends Piece
-{
-   @Override
-   void possibleMoves()
-   {
-      System.out.println("Temp list Bishop command");
-   }
-   
-   //Add Position newPos later.
-   @Override
-   void move()
-   {
-      System.out.println("Temp move Bishop command");
-   }
+import Utils.Utils;
+
+public class Bishop extends Piece {
+
+    public Bishop(int color, String position) {
+        this.color = color;
+        this.position = position;
+    }
+
+    @Override
+    public void possibleMoves() {
+        System.out.println("Possible Bishop moves from " + position + ":");
+
+        int[] pos = Utils.toRowCol(position);
+        int row = pos[0];
+        int col = pos[1];
+
+        int[][] directions = {
+            {-1, -1}, {-1, 1}, // upper-left, upper-right
+            {1, -1}, {1, 1}    // lower-left, lower-right
+        };
+
+        for (int[] dir : directions) {
+            int r = row;
+            int c = col;
+
+            while (true) {
+                r += dir[0];
+                c += dir[1];
+
+                if (r < 0 || r >= 8 || c < 0 || c >= 8)
+                    break;
+
+                String targetPos = Utils.toPositionString(r, c);
+
+                // For now, just print possible squares
+                // In Phase 2, you'd check board for collisions and captures
+                System.out.println("Can move to: " + targetPos);
+            }
+        }
+    }
+
+    @Override
+    public void move() {
+        System.out.println("Bishop moved to new position (to be implemented).");
+    }
 }
--- Pieces/Rook.java
+++ Pieces/Rook.java
@@ -1,17 +1,58 @@
 package Pieces;
 
-public class Rook extends Piece
-{
-   @Override
-   void possibleMoves()
-   {
-      System.out.println("Temp list Rook command");
-   }
-   
-   //Add Position newPos later.
-   @Override
-   void move()
-   {
-      System.out.println("Temp move Rook command");
-   }
+import java.util.ArrayList;
+import java.util.List;
+
+import Board.Board; // Assuming you’ll have a Board class with getPiece(row, col)
+import Utils.Utils;
+
+public class Rook extends Piece {
+
+    public Rook(int color, String position) {
+        this.color = color;
+        this.position = position;
+    }
+
+    @Override
+    public void possibleMoves() {
+        System.out.println("Possible Rook moves from " + position + ":");
+
+        int[] pos = Utils.toRowCol(position);
+        int row = pos[0];
+        int col = pos[1];
+
+        int[][] directions = {
+            {-1, 0}, {1, 0}, // up, down
+            {0, -1}, {0, 1}  // left, right
+        };
+
+        for (int[] dir : directions) {
+            int r = row;
+            int c = col;
+
+            while (true) {
+                r += dir[0];
+                c += dir[1];
+
+                if (r < 0 || r >= 8 || c < 0 || c >= 8)
+                    break;
+
+                String targetPos = Utils.toPositionString(r, c);
+
+                // You’ll need to reference the board later
+                System.out.println("Can move to: " + targetPos);
+
+                // When Board is implemented:
+                // Piece target = board.getPiece(r, c);
+                // if (target == null) { add targetPos to list }
+                // else if (target.color != this.color) { add and break }
+                // else break;
+            }
+        }
+    }
+
+    @Override
+    public void move() {
+        System.out.println("Rook moved to new position (to be implemented).");
+    }
 }
