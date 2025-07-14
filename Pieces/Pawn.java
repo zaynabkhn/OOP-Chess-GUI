bfaefafb8cd512466a37ffd72468f664778fd11f
qgy13 <qgy13@txstate.edu>
2025-07-13 23:17:42 -0500

    Adding some Javadocs to Pawn.java

--- Pieces/Pawn.java
+++ Pieces/Pawn.java
@@ -1,13 +1,26 @@
 package Pieces;
 
+/**
+* This is the Pawn piece class that will have the movement
+* cycles and options for a Pawn in a Chess game.
+* 
+* This is an extention of the abstract class Piece.
+*/
 public class Pawn extends Piece
 {
+   /**
+   * possibleMoves() lists the possible moves that a Pawn can make.
+   */
    @Override
    void possibleMoves()
    {
       System.out.println("Temp list command");
    }
-   
+
+   /**
+   * move() allows the player to move in ordinance with the
+   * Pawn Chess piece 
+   */
    //Add Position newPos later.
    @Override
    void move()
