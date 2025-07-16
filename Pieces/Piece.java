f8a2586b56ee670ef86450e522229a1f33e7790e
qgy13 <qgy13@txstate.edu>
2025-07-16 17:58:20 -0500

    possibleMoves() Javadoc for Piece.java

--- Pieces/Piece.java
+++ Pieces/Piece.java
@@ -6,7 +6,13 @@ public abstract class Piece
 {
    int color;
    String position;
-   
+
+   /**
+     * Lists the possible moves of a chess piece.
+     *
+     * @param - none
+     * @return - none
+     */
    abstract void possibleMoves();
    
    abstract void move(Position newPos);
